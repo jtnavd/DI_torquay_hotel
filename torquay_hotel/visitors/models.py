ec2f9da8545379d3e276ff12815a29e49565df56
lidu <git@hebi.fr>
2021-06-03 08:38:12 +0300

    adjusted visitors/models.py

--- torquay_hotel/visitors/models.py
+++ torquay_hotel/visitors/models.py
@@ -26,19 +26,19 @@ class Room(models.Model):
 
 
 class Picture(models.Model):
-    image = models.ImageField(upload_to='pictures/', null = True, blank = True)
-    hotel_id = models.ForeignKey(Hotel, null = True,  on_delete = models.PROTECT)
-    room_id = models.ManyToManyField(Room, null = True, on_delete = models.PROTECT)
+    image = models.ImageField(upload_to='pictures/', null=True, blank=True)
+    hotel_id = models.ForeignKey(Hotel, null=True, on_delete= models.PROTECT)
+    room_id = models.ManyToManyField(Room)
 
 
 class Guest(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birthdate = models.DateField()
-    nationality = models.ForeignKey(Country)
+    nationality = models.ForeignKey(Country, on_delete= models.PROTECT)
     passport_nb = models.CharField(max_length=20)
     address = models.CharField(max_length=200, null=True, blank=True)
-    telephone = models.charField(max_length=20)
+    telephone = models.CharField(max_length=20)
     email = models.EmailField()
 
 
@@ -49,7 +49,7 @@ class Booking(models.Model):
     canceled: by staff member"""
     start_day = models.DateField() # start of stay
     end_day = models.DurationField() # end of stay
-    room_id = models.ManyToManyField(Room, related_name = 'rooms', null=True, blank=True)
+    room_id = models.ManyToManyField(Room, related_name = 'rooms', blank=True)
     guest_id = models.ForeignKey(Guest, on_delete = models.CASCADE)
     status = models.CharField(max_length=10, default = 'pending')
     creation_date = models.DateTimeField(auto_now_add=True)
